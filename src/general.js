5b749b818f9cce09fd0405e7a30accb3ee957e3e
DimaD1988 <dimdon88@gmail.com>
2023-11-08 14:49:00 +0700

    feat: add sticky navigation

--- src/general.js
+++ src/general.js
@@ -1,15 +1,30 @@
-const headerEL = document.querySelector('header .container');
+const headerEL = document.querySelector('header');
 const navList = document.querySelector('.nav-list');
 const menuBtn = document.getElementById('menu');
-/* //// Navigation //// */
+const secHeroEl = document.getElementById('sec-hero');
+/* //// Mobile Navigation //// */
 let navListIsOpen = false;
 menuBtn.addEventListener('click', () => {
 	headerEL.classList.add('nav-open');
 	navListIsOpen = true;
 });
 navList.addEventListener('click', () => {
-	if (navListIsOpen === true) {
+	if (navListIsOpen) {
 		headerEL.classList.remove('nav-open');
 		navListIsOpen = false;
 	}
 });
+/* //// Sticky Navigation //// */
+const navObs = new IntersectionObserver(
+	(entries) => {
+		entries[0].isIntersecting
+			? headerEL.classList.remove('sticky')
+			: headerEL.classList.add('sticky');
+	},
+	{
+		root: null,
+		threshold: 0,
+		rootMargin: '-120px',
+	}
+);
+navObs.observe(secHeroEl);
