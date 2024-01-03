857f578fb8ec3e598c9a8a4a3f4785b0e6d76801
DimaD1988 <dimdon88@gmail.com>
2024-01-03 20:28:22 +0300

    feat: add homePage tourOffer pagination

---
+++ src/js/tourPages.js
@@ -0,0 +1,50 @@
+'use strict';
+/* //// Tour Pages //// */
+function tourPages() {
+	// Variables
+	const tourDaysSelector = ['.d1', '.d3n2', '.d4n3', '.d5n4'];
+	const leftBtn = document.querySelector('.btn-left');
+	const rightBtn = document.querySelector('.btn-right');
+	let tourPage = 0;
+
+	// Functions
+	function addNone(curTour) {
+		tourDaysSelector.forEach((tour, i) => {
+			if (curTour !== i) {
+				document.querySelector(`${tour}`).classList.add('none');
+			} else {
+				document.querySelector(`${tour}`).classList.remove('none');
+			}
+		});
+	}
+
+	function prevTourPage() {
+		if (tourPage === 0) {
+			tourPage = tourDaysSelector.length - 1;
+			addNone(tourDaysSelector.length - 1);
+		} else {
+			tourPage--;
+			addNone(tourPage);
+		}
+	}
+
+	function nextTourPage() {
+		if (tourPage === tourDaysSelector.length - 1) {
+			tourPage = 0;
+			addNone(0);
+		} else {
+			tourPage++;
+			addNone(tourPage);
+		}
+	}
+
+	function initTourPage() {
+		addNone(0);
+	}
+	initTourPage();
+
+	// Event Handlers
+	leftBtn.addEventListener(`click`, prevTourPage);
+	rightBtn.addEventListener(`click`, nextTourPage);
+}
+tourPages();
